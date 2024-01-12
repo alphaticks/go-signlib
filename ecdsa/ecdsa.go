0aa6b6e2fb6f5ee08104a839bbd195bab5ebbe82
Laurent Zeimes <laurent@alphaticks.io>
2024-01-12 18:20:10 +0100

    fix: copy bytes

--- ecdsa/ecdsa.go
+++ ecdsa/ecdsa.go
@@ -7,16 +7,29 @@ package ecdsa
 
 // Define the struct to match the Rust ExtendedSignatureC struct
 typedef struct {
-    const void* r;
-    const void* s;
+    uint8_t r[32];
+    uint8_t s[32];
 } SignatureC;
+
+typedef struct {
+    const void* e;
+} HashC;
+
 
 // Function pointer type for sign_ecdsa
 typedef int32_t (*sign_ecdsa_type)(const void*, const void*, SignatureC*);
 
+// Function pointer type for hash_pedersen
+typedef int32_t (*hash_pedersen_type)(const void*, const void*, HashC*);
+
 // Wrapper function to call sign_ecdsa
 int32_t sign_ecdsa(void* f, const void* private_key, const void* message, SignatureC* output) {
     return ((sign_ecdsa_type) f)(private_key, message, output);
+}
+
+// Wrapper function to call hash_pedersen
+int32_t hash_pedersen(void* f, const void* e0, const void* e1, HashC* output) {
+    return ((hash_pedersen_type) f)(e0, e1, output);
 }
 */
 import "C"
@@ -28,6 +41,7 @@ import (
 
 var signSTARKPtr unsafe.Pointer
 var signEd25519Ptr unsafe.Pointer
+var hashPedersenPtr unsafe.Pointer
 
 func Load(path string) {
 	handle := C.dlopen(C.CString(path), C.RTLD_LAZY)
@@ -41,6 +55,10 @@ func Load(path string) {
 	signEd25519Ptr = C.dlsym(handle, C.CString("sign_ed25519"))
 	if signEd25519Ptr == nil {
 		panic("function sign_ed25519 not found in the library")
+	}
+	hashPedersenPtr = C.dlsym(handle, C.CString("hash_pedersen"))
+	if hashPedersenPtr == nil {
+		panic("function hash_pedersen not found in the library")
 	}
 }
 
@@ -61,8 +79,32 @@ func SignSTARK(msgHash, privKey *big.Int) (r, s *big.Int, err error) {
 	} else if res == -2 {
 		return nil, nil, fmt.Errorf("invalid private key")
 	}
-	r = new(big.Int).SetBytes((*[32]byte)(signature.r)[:])
-	s = new(big.Int).SetBytes((*[32]byte)(signature.s)[:])
+	rBytes := C.GoBytes(unsafe.Pointer(&signature.r[0]), 32)
+	sBytes := C.GoBytes(unsafe.Pointer(&signature.s[0]), 32)
+	r = new(big.Int).SetBytes(rBytes)
+	s = new(big.Int).SetBytes(sBytes)
+	//fmt.Println("S", s.String())
+	return
+}
+
+func HashPedersen(e0, e1 *big.Int) (e *big.Int, err error) {
+	if hashPedersenPtr == nil {
+		return nil, fmt.Errorf("library not loaded")
+	}
+	var hash C.HashC
+	var e0Bytes, e1Bytes [32]byte
+	e0.FillBytes(e0Bytes[:])
+	e1.FillBytes(e1Bytes[:])
+	//fmt.Println(privKeyBytes)
+	//fmt.Println(fmt.Sprintf("%p, %p, %p, %p", signEcdsaPtr, unsafe.Pointer(&privKeyBytes[0]), unsafe.Pointer(&msgHashBytes[0]), unsafe.Pointer(&signature)))
+	//res := trempoline.CallCFunc(uintptr(signEcdsaPtr), uintptr(unsafe.Pointer(&privKeyBytes[0])), uintptr(unsafe.Pointer(&msgHashBytes[0])), uintptr(unsafe.Pointer(&signature)))
+	res := C.hash_pedersen(hashPedersenPtr, unsafe.Pointer(&e0Bytes[0]), unsafe.Pointer(&e1Bytes[0]), &hash)
+	if res == -1 {
+		return nil, fmt.Errorf("invalid arguments")
+	} else if res == -2 {
+		return nil, fmt.Errorf("invalid private key")
+	}
+	e = new(big.Int).SetBytes((*[32]byte)(hash.e)[:])
 	return
 }
 
--- ecdsa/ecdsa_test.go
+++ ecdsa/ecdsa_test.go
@@ -1,15 +1,23 @@
 package ecdsa
 
 import (
+	"fmt"
 	"math/big"
 	"testing"
 )
 
 func TestEcdsa(t *testing.T) {
+	Load("/home/loracle/Documents/alphaticks/go-signlib/lib/sign/target/release/libsign.so")
 	priv, _ := new(big.Int).SetString("2694640734420098289168932321453844877445981823409726840565151125758414758195", 10)
 	k, _ := new(big.Int).SetString("1860628535426984640718681250822843799056280994042392077290961110599908043918", 10)
 	_, _, err := SignSTARK(k, priv)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	res, err := HashPedersen(priv, k)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(res.String())
 }
